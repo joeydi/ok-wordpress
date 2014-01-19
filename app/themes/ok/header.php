7143f7b5fffcd1aa2bb747d252c12b2cd9efb24e
Joe di Stefano <joeydi@okaypl.us>
2014-01-19 00:20:07 -0500

    remove duplicate sitename from page header

--- app/themes/ok/header.php
+++ app/themes/ok/header.php
@@ -8,7 +8,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php wp_title( '|', true, 'right' ); bloginfo('name'); ?></title>
+        <title><?php wp_title( '|', true, 'right' ); ?></title>
         <link rel='shortcut icon' type='image/x-icon' href='<?php echo get_bloginfo( 'stylesheet_directory' ) . '/images/favicon.png'; ?>' />
         <?php wp_head(); ?>
     </head>
