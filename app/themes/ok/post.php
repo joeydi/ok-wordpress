c1b4d977e3397aee647c61f73cb4facb0522cbb3
Joe di Stefano <joeydi@okaypl.us>
2014-02-17 21:55:09 -0500

    add image attribution to featured images

--- app/themes/ok/post.php
+++ app/themes/ok/post.php
@@ -3,6 +3,23 @@
     <?php if ( has_post_thumbnail() ) : ?>
     <div class="post-image">
         <?php the_post_thumbnail(); ?>
+        <?php $post_thumbnail_id = get_post_thumbnail_id(); ?>
+        <?php $source_name = get_field( 'source_name', $post_thumbnail_id ); ?>
+        <?php $source_url = get_field( 'source_url', $post_thumbnail_id ); ?>
+
+        <?php if ( $source_name ) : ?>
+        <div class="image-source">
+            <span>Photo:</span>
+
+            <?php if ( $source_url ) : ?>
+            <a href="<?php the_field( 'source_url', $post_thumbnail_id ); ?>">
+            <?php echo $source_name; ?>
+            </a>
+            <?php else : ?>
+            <span><?php echo $source_name; ?></span>
+            <?php endif; ?>
+        </div>
+        <?php endif; ?>
     </div>
     <?php endif; ?>
 
