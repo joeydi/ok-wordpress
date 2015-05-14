11ca03d8e2537d5fc430b4ae29ee525169755fe8
Joe di Stefano <joeydi@okaypl.us>
2015-05-14 16:11:24 -0400

    add default page template

--- wp-content/themes/ok/index.php
+++ wp-content/themes/ok/index.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 
-
+<div id="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1">
+                <?php while ( have_posts() ) : the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
