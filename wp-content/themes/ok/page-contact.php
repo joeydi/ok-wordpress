b8799cfad7c9200c8df60f577a86d8f6a2999df2
Joe di Stefano <joeydi@okaypl.us>
2015-06-29 17:03:05 -0400

    update office address

--- wp-content/themes/ok/page-contact.php
+++ wp-content/themes/ok/page-contact.php
@@ -40,7 +40,7 @@
                     <h2>Send Mail To:</h2>
                     <p>
                         <strong>Okay Plus</strong><br />
-                        47 Maple St, Suite 106<br />
+                        22 Church St, Suite 306<br />
                         Burlington, VT 05401
                     </p>
                 </div>
